0ec490edef6b282bc6eae09754fabc5fcf2ac8c2
cha-verde <novitrna@gmail.com>
2024-11-28 09:38:42 +0700

    Cleanup for template

--- src/reservation-content.js
+++
@@ -1,6 +0,0 @@
-const reservationContent = document.createElement("p");
-reservationContent.setAttribute("class", "reservation-section");
-
-reservationContent.textContent = "To make reservations, please contact +1 555 879 0011"
-
-export {reservationContent}
